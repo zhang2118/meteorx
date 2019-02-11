c4b0f803f520225cc1d21a66bf2f25a4d077f8be
Wensheng ZHANG <wensheng.zhang@instant.co>
2019-02-11 09:50:30 -0500

    downgrade package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Exposing Internal Meteor Apis to Hack Meteor Easily",
-  version: "2.0.1",
+  version: "1.0.2",
   git: "https://github.com/lamhieu-vk/meteorx.git",
   name: "meteorhacks:meteorx"
 });
